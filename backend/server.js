3c48f7fdcebc8cb5b5a2292a3dc2c2f7722417f0
jbullis0255 <49146512+jbullis0255@users.noreply.github.com>
2021-10-21 10:53:46 +0300

    backend

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,15 @@ connectDb()
 //setting up config file
 dotenv.config({path:'backend/config/config.env'})
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
+})  
+
+//handle unhandled promise Rejections
+process.on('unhandledRejection', err=>{
+    console.log(`ERROR: ${err.message}`)
+    console.log('Server shutting down due to Unhandled Promise Rejection')
+    server.close(()=>{
+        process.exit(1)
+    })
 })
