4db380685b2a78b55a04f21bc4a2dd35b3f68f03
jbullis0255 <49146512+jbullis0255@users.noreply.github.com>
2021-10-29 11:27:53 +0300

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header1 from './components/layout/Header1'
 import Header2 from './components/layout/Header2'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header1/>
       <Header2/>
+      <Footer/>
 
     </div>
   );
--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { Fragment } from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <Fragment>
+      <footer className="py-1">
+        <p className="text-center  mt-1">
+         LegalHelp Online 2021-2022, All Rights Reserved
+        </p>
+      </footer>
+    </Fragment>
+  );
+};
 
-export default Footer
+export default Footer;
