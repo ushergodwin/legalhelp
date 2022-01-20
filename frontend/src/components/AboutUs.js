4d4a4452324e48f7dbf55d6752fbb5d4d571c02e
jbullis0255 <49146512+jbullis0255@users.noreply.github.com>
2022-01-20 22:06:55 +0300

    worked on about-us and why-us

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -11,12 +11,13 @@ const AboutUs = () => {
           
           </p> */}
           <div id="about-image">
-            <img src={"/imgs/about-slider.png"} alt="aboutimage" />
+            <img className="img-fluid max-width:100% pb-2" src={"/imgs/about-slider.png"} alt="aboutimage" />
           </div>
         </div>
         <div className="about"> 
-        <h2>About Us</h2>
-       
+        <br/>
+        <h2><u>About Us</u></h2>
+       <br/>
         <p id="about-message">
           legalHelpOnline works to ensure everyone has access to justice. Our
           experts teams work across practice areas on nearly every area of law
@@ -24,13 +25,13 @@ const AboutUs = () => {
           courts and post conviction matters. We are dedicated to ensuring that
           every person has equal access to justice. We fight for justice in
           courts and outside courts through outreach, education campaigns and
-          legal representation. We provide civil and criminal legal aid and
+          legal representation.<br/><br/>We provide civil and criminal legal aid and
           advice to help people deal with their legal problems. We present
           children and youth in courts. We provide representation, legal
           assistance, and education to low wage and unemployed workers who have
           experienced violations of the employment laws. We are a team of
           passionate professionals and legal experts dedicated to the cause of
-          access to justice.<br/> <b>Talk to us on +256 784414719/ +256 706142727 or
+          access to justice.<br/><br/> <b>Talk to us on +256 784414719/ +256 706142727 or
           share your view with us by email</b>
         </p>
         </div>
